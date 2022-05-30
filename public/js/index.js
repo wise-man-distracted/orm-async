bff61e09cc67764031647fb297f54792a95d8877
wise-man-distracted <magewelle@live.com>
2022-05-30 20:00:47 -0300

    views incomplete

---
+++ public/js/index.js
@@ -0,0 +1,18 @@
+console.log('estoy siendo exiecutado')
+
+
+let listaDeAmigos = document.getElementById('listaDeAmigos');
+let amigos = [{
+    id: 1,
+    nome: "Wendel Cultrim",
+    email: "wendel@digitalhouse.com"
+}, {
+    id: 2,
+    nome: "Sérgio Moura",
+    email: "ssiqueira@digitalhouse.com"
+}, {
+    id: 3,
+    nome: "Silvia Fiacador",
+    email: "silvia@digitalhouse.com"
+}]
+console.log(listaDeAmigos);
