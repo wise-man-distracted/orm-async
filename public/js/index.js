15ab2c16d72027652ca7f99a7c8ddb9ca790c2db
wise-man-distracted <magewelle@live.com>
2022-05-31 22:49:35 -0300

    syncing

--- public/js/index.js
+++ public/js/index.js
@@ -16,3 +16,17 @@ let amigos = [{
     email: "silvia@digitalhouse.com"
 }]
 console.log(listaDeAmigos);
+
+let string = ""
+for (let i = 0; i < amigos.length; i++) {
+    const amigo = amigos[i];
+    string += `
+    <article>
+          <img src="img/face.jpg" alt="${amigo.nome}">
+          <span>${amigo.nome}</span>
+          <a href="${amigo.email}">${amigo.email}</a>
+    </article>
+    `
+}
+
+listaDeAmigos.innerHTML = string
